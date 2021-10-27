e7cbdcf351b97a7d7017bc8795d94c34a00dd396
AngeleLindarteL <angelgamerlolwtf@gmail.com>
2021-10-26 21:12:23 -0500

    modified response on CategoryView

--- authGestifyApp/views/categoryView.py
+++ authGestifyApp/views/categoryView.py
@@ -5,12 +5,12 @@ from rest_framework.response import Response
 class CategoryView(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kwargs):
-        categories = {
-            1: ('Beb', 'Bebidas'),
-            2:('Mas', 'Para_Mascotas'),
-            3: ('Beb', 'Para_Bebés'),
-            4: ('AP',  'Aseo_Personal'),
-            5: ('Lim', 'Limpieza'),
-        }
+        categories =(
+            {"cod" :'Beb', "name": "Bebidas"},
+            {"cod" :'Mas', "name": "Para_Mascotas"},
+            {"cod" :'Beb', "name": "Para_Bebés"},
+            {"cod" :'AP',  "name": "Aseo_Personal"},
+            {"cod" :'Lim', "name": "Limpieza"},
+        )
         
         return Response(categories,status=status.HTTP_200_OK)
